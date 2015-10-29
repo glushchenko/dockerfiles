fdc0f35d1452c1714531dbcce9aacc38d37493f8
glushchenko <glushchenko@users.noreply.github.com>
2015-10-29 22:49:13 +0200

    Update bootstrap.sh

--- znc/bootstrap.sh
+++ znc/bootstrap.sh
@@ -3,9 +3,11 @@
 version="1.6.1"
 
 apt-get update
-apt-get install -y build-essential libssl-dev libperl-dev pkg-config ca-certificates wget
+apt-get install -y build-essential libssl-dev libperl-dev pkg-config ca-certificates wget libicu-dev
 
 useradd znc
+
+# Build znc
 
 mkdir -p /var/run/znc/ && chmod 777 /var/run/znc/
 mkdir -p /src/znc && cd /src/znc
@@ -15,8 +17,17 @@ tar -zxf "znc-${version}.tar.gz"
 cd "znc-${version}"
 ./configure && make && make install
 
+# Build and install clientbuffer
+
+cd /src/znc/
+wget https://raw.githubusercontent.com/jpnurmi/znc-clientbuffer/master/clientbuffer.cpp
+znc-buildmod clientbuffer.cpp
+mv clientbuffer.so /usr/local/lib/znc
+
+# Clean
+
+rm -rf /src/znc/*
+
 apt-get remove -y wget
 apt-get autoremove -y
 apt-get clean
-
-rm -rf /src/znc/*
