c8a796025f98b9d1facb911e642709c52261ae98
Oleksandr Glushchenko <contact@fluder.co>
2015-11-06 16:33:36 +0200

    common entrypoint

---
+++ znc/entrypoint.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+DATADIR="/znc-data"
+
+# Build modules from source.
+if [ -d "${DATADIR}/modules" ]; then
+  cwd="$(pwd)"
+
+  modules=$(find "${DATADIR}/modules" -name "*.cpp")
+
+  for module in $modules; do
+    cd "$(dirname "$module")"
+    /usr/local/bin/znc-buildmod "$module"
+  done
+
+  cd "$cwd"
+fi
+
+chown -R znc:znc "$DATADIR"
+
+/usr/local/bin/znc -f --datadir="$DATADIR" $@
